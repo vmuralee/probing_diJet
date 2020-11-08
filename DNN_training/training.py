fix normalisation

DNN_training/training.py
@@ -5,7 +5,6 @@
 
 import os
 import ROOT
-import tensorflow as tf
 from tensorflow import keras
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -19,7 +18,6 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from scipy.stats import reciprocal
 #from sklearn.model_selection import RandomizedSearchCV
 
 parser = argparse.ArgumentParser(description='Training DNN model')
@@ -29,7 +27,7 @@ parser.add_argument('--cat',type=str,help='select categories',default='qg')
 parser.add_argument('--path',type=str,help='pathe of samples',default='../data/Ntuples')
 parser.add_argument('--JSS',type=bool,help='add JSS parameter',default=False)
 parser.add_argument('--csv',type=bool,help='save csv file',default=False)
-parser.add_argument('--outfile',type=str,help='output root file',default='test.root')
+parser.add_argument('--outfile',type=str,help='output root file',default='test')
 parser.add_argument('--cut',type=int,help='Invariant mass cut',default=(0,10000),nargs=2)
 args = parser.parse_args()
 if(args.cat not in ['qq','qg','gg']):
@@ -42,15 +40,20 @@ cat_files = dict({'qq':['pp2jj_SMbkg-Nev2e6-ptJetMin700-antiktR0.4-zcut0.1-beta1
 })
 filenames = cat_files[args.cat]
 
+prefix = '_pt1_'+args.cat
+
 labels = ['ptJ1','ptJ2','invmass','DRJ1J2','etaJ1','etaJ2']
 if(args.JSS):
+    prefix = prefix+'JSS'
     print('Using JSS paramets')
     labels = labels+['pTDJ1','pTDJ2','LHAJ1','LHAJ2','e05J1','e05J2','s2J1','s2J2','pmJ1','pmJ2','tmJ1','tmJ2','widthJ1','widthJ2','girthJ1','girthJ2']
 
 m_cut = args.cut
-if(m_cut[0]==0 and m_cut == 10000):
+if(m_cut[0]==0 and m_cut[1] == 10000):
+    prefix = prefix+'_Nocut'
     print('No invariant mass cut')
 else:
+    prefix = prefix+'minv_'+str(m_cut[0])+'_'+str(m_cut[1])
     print('Invariant mass cut of ',m_cut[0],' < m_inv < ',m_cut[1],' applied')
 data = Data2Array(args.path,filenames,labels,m_cut)
 data_ar = data.load_df()
@@ -62,7 +65,7 @@ if(args.JSS):
 if(args.csv):
     data.save_csv(data_ar,savefileAs)
 
-
+    
 #Creating Training,Test and Validation samples
 traindataset_full,testdataset = train_test_split(data_ar,test_size=0.1,random_state=42)
 traindataset,valdataset = train_test_split(traindataset_full,test_size=0.2,random_state=42)
@@ -103,19 +106,46 @@ l_rate =  0.01304080507013371
 #Tuned Model
 model = build_model(n_hidden=3,n_neurons=475,learning_rate=l_rate,input_shapes=len(labels))
 
+print('training model for '+prefix+' ..............................')
 #checkpoint_cb = keras.callbacks.ModelCheckpoint("keras_model.h5")
 prefit = model.fit(X_train_tr,y_train,epochs=args.epoch,validation_data=(X_val_tr,y_val),callbacks=[keras.callbacks.EarlyStopping(patience=10)])
-if(args.JSS):
-    model.save("keras_model_JSS_"+args.cat+"_wcut.h5")
-else:
-    model.save("keras_model_"+args.cat+"_wcut.h5")
+
+model_dir = "../output/models/model_"+prefix
+try:
+    os.mkdir(model_dir)
+except:  
+    print('Folder already created')
+model.save(model_dir+"/keras_model_"+prefix+".h5")
+
 
 #Learning Curves
 pd.DataFrame(prefit.history).plot(figsize=(8,5))
+plt.figure(1)
 plt.grid(True)
 plt.gca().set_ylim(0,1)
-plt.savefig('../plots/learning_Curves.png')
+plt.savefig(model_dir+"/learning_curve.png")
 #plt.show()
+
+rootfile = model_dir+'/'+args.outfile+prefix+'.root'
+
+#Training ROC
+y_tr_pred = model.predict(X_train_tr)
+plt.figure(2)
+plt.plot(figsize=(6,5))
+plt.grid(True)
+y_out_tr = np.array(y_train,dtype='float32')
+fpr_tr,tpr_tr,th_tr = roc_curve(y_true=y_out_tr,y_score=y_tr_pred[:,1])
+auc = roc_auc_score(y_out_tr,y_tr_pred[:,1])
+plt.plot(tpr_tr,1-fpr_tr)
+plt.text(0.15,0.35,f'auc value: {auc}')
+plt.xlabel('efficiency')
+plt.ylabel('misidentification')
+plt.savefig(model_dir+'/training_ROC_'+prefix+'.png')
+
+file_ = ROOT.TFile(rootfile,"update")
+gr_tr = ROOT.TGraph(fpr_tr.size,tpr_tr,1-fpr_tr)
+gr_tr.Write("training_roc")
+file_.Close()
 
 #Prediction
 if(args.predict):
@@ -123,18 +153,14 @@ if(args.predict):
     
 #Storing ntuple
 if(args.predict):
-    tfile = Array2Data(y_pred,testdataset,args.outfile)
-    prefix_ = args.cat
-    if(args.JSS):
-        prefix_=prefix_+"JSS"
-    tfile.SigTree(prefix_)
+    print('Creating outfile file '+rootfile+' .......................................')
+    tfile = Array2Data(y_pred,testdataset,rootfile)
+    tfile.SigTree()
     tfile.BkgTree()
 
 #ROC
 if(args.predict):
-    prefix_ = args.cat
-    if(args.JSS):
-        prefix_=prefix_+"JSS"
+    plt.figure(3)
     plt.plot(figsize=(6,5))
     plt.grid(True)
     y_out = np.array(y_test,dtype='float32')
@@ -142,13 +168,13 @@ if(args.predict):
     auc = roc_auc_score(y_out,y_pred[:,1])
     plt.plot(tpr,1-fpr)
     plt.text(0.15,0.35,f'auc value: {auc}')
-    plt.savefig('../plots/ROC_'+prefix_+'.png')
     plt.xlabel('efficiency')
     plt.ylabel('misidentification')
+    plt.savefig(model_dir+'/ROC_'+prefix+'.png')
     #plt.show()
 
     #Storing ROC
-    file_ = ROOT.TFile(args.outfile,"update")
+    file_ = ROOT.TFile(rootfile,"update")
     gr = ROOT.TGraph(fpr.size,tpr,1-fpr)
-    gr.Write("roc_graph")
+    gr.Write("test_roc")
     file_.Close()

analyse/plot_cl.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize as opt;
-
+import math
 improve = True
 def func(x,a,b,c,d,e):
     return a*x**4+b*x**3+c*x**2+d*x+e
@@ -17,6 +17,7 @@ with open('sigma_JSS.txt') as f:
 with open('sigma.txt') as fg:
     for line in fg:
         val = line.split()
+        #print(val)
         s_list.append(float(val[-1]))        
         
 bsyst = np.linspace(0.005,0.05,50)
@@ -25,14 +26,19 @@ sigma_JSS = np.array(s_list_JSS)
 
 sig_imp = sigma_JSS/sigma
 optimizedParameters, pcov = opt.curve_fit(func,bsyst,sig_imp);
+optimizedParameters1, pcov1 = opt.curve_fit(exp_func,bsyst,sigma_JSS);
+optimizedParameters2, pcov2 = opt.curve_fit(exp_func,bsyst,sigma);
 
+sig_imp_fit = exp_func(bsyst,*optimizedParameters1)/exp_func(bsyst,*optimizedParameters2)
 if improve==True:
-    plt.scatter(bsyst*100,sig_imp,label='sigma_JSS/sigma',c='navy',alpha=0.2)
-    plt.plot(bsyst*100, func(bsyst, *optimizedParameters),c='g', label="fit")
+    #plt.scatter(bsyst*100,sig_imp,label='sigma_JSS/sigma',c='g')
+    #plt.plot(bsyst*100, sig_imp_fit,c='g', label="sigma_JSS/sigma")
+    plt.plot(bsyst*100,func(bsyst,*optimizedParameters),c='g', label="sigma_JSS/sigma")
 else:
-    plt.scatter(bsyst*100,sigma,label='without JSS')
-    plt.scatter(bsyst*100,sigma_JSS,label='with JSS')
-
+    # plt.scatter(bsyst*100,sigma,label='without JSS')
+    # plt.scatter(bsyst*100,sigma_JSS,label='with JSS')
+    plt.plot(bsyst*100,exp_func(bsyst,*optimizedParameters1),c='b', label="wtih JSS")
+    plt.plot(bsyst*100,exp_func(bsyst,*optimizedParameters2),c='r', label="wtihout JSS")
 
 plt.xlabel('relative uncertainity [%]')
 

analyse/script_forCL.py
@@ -9,7 +9,7 @@ with open('events.txt') as f:
 
 #print(Sevent_list)
 nbins = len(Bevent_list)
-bsyst = np.linspace(0.005,0.05,50)
+bsyst = np.linspace(0.005,0.05,20)
 
 def createline(list_):
     line = ''